c7c0b21c57eeffa81aab3a2c265a51e91e5841dd
ng1n3 <code.with.muyiwa@gmail.com>
2024-07-05 00:35:37 +0100

    mini app v1

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+const app: Application = express();
+
+app.use(express.json());
+
+app.use("/api/v1/users", (req: Request, res: Response) => {
+  res.status(200).send({
+    status: "success",
+    message: "Welcome to users",
+  });
+});
+
+app.use("*", (req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({
+    status: "Error",
+    message: "resource not available 😒",
+  });
+});
+
+app.listen(8090, () => {
+  console.log("App listening on http://localhost:80");
+});
